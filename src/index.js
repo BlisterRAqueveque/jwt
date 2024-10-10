73adf1f365fad76ab558413fddc26b5c3377ab01
Roberto Aqueveque <141036948+BlisterRAqueveque@users.noreply.github.com>
2024-10-09 22:32:52 -0300

    Fix!: Fx index.js

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const io = new Server(server);
 //const socketHandler = require("./sockets/handler.socket");
 
 let sockets = [];
+let socketsInfo = [];
 
 const main = () => {
   server.listen(app.get("port"));
@@ -23,16 +24,35 @@ const main = () => {
   io.on("connection", (socket /* La conexión del usuario */) => {
     console.log(`Usuario conectado: ${socket.id}`);
 
-    sockets.push({
-      id: socket.id,
+    sockets.push(socket);
+    socketsInfo.push({
+      socket: socket.id,
       nombre: socket.handshake.headers
         ? socket.handshake.headers.nombre
+        : "No tiene nombre de usuario",
+      otro: socket.handshake.headers
+        ? socket.handshake.headers.otro
         : "No tiene nombre de usuario",
     });
 
     // socketHandler.socketHandler(socket);
+    socket.on("nuevo-recibo", (data) => {
+      io.emit("nuevo-recibo-carga", JSON.stringify(data));
+      // sockets.forEach((s) => {
+      //   s.emit("nuevo-recibo-carga", JSON.stringify(data));
+      // });
+    });
+    //sockets[0].emit("usuario", JSON.stringify(socketsInfo));
 
-    io.emit("usuario", JSON.stringify(sockets));
+    sockets[0].on("hola", (data) => {
+      const nombre = socketsInfo.find(
+        (i) => i.nombre == socket.handshake.headers.otro
+      );
+      if (nombre) {
+        const otro = sockets.find((i) => i.id == nombre.socket);
+        otro.emit("recibe-hola", data);
+      }
+    });
 
     socket.on("disconnect", () => {
       console.log("Usuario desconectado");
